a22e346649e73184c4bf80035d90d5f5c2b6bb78
alex-Symbroson <alex.symbroson@gmail.com>
2017-10-01 22:21:14 +0200

    Add files via upload

--- BotScript/src/pins.cpp
+++ BotScript/src/pins.cpp
@@ -1,16 +1,45 @@
 
-//$ g++ -std=c++11 -O3 -c pins.cpp
+//c++ -std=c++11 -O3 -lwiringPi -o pins.out pins.cpp;./pins.out
 
-#include "pins.hpp"
-#include <wiringPi.h> //-
-#include <softPwm.h>
+#ifndef MAIN
+	#define MAIN 3
+	int main();
+#endif
 
-/* softPwm.h
-	extern int  softPwmCreate (int pin, int value, int range);
-	extern void softPwmWrite  (int pin, int value);
-	extern void softPwmStop   (int pin);
-*/
+
+#include<wiringPi.h>//- 
+#include "extern.h"
+
+extern "C" {
+	//<softPwm.h>
+	extern intsoftPwmCreate (int pin, int value, int range);
+	extern void softPwmWrite(int pin, int value);
+	extern void softPwmStop (int pin);
+}
 
 void setup() {
-	if(wiringPiSetup() == - 1) Error::error("setup wiringPi failed !");
+	if(wiringPiSetup() == - 1) error("setup wiringPi failed !");
 }
+
+
+#if MAIN == 3
+
+int main() {
+	setup();
+
+	int ledRed = 0;
+	softPwmCreate(ledRed,0, 255);
+
+	for(int i = 0;i<3;i++) {
+		softPwmWrite(ledRed, 255);
+		delay(1000);
+		softPwmWrite(ledRed, 0);
+		delay(1000);
+	}
+
+	softPwmStop(ledRed);
+
+	return 0;
+}
+
+#endif
