0697b8150e6ea111f6e9f02eca84672a368b3148
alex-Symbroson <alex.symbroson@gmail.com>
2017-10-02 15:25:13 +0200

    better structure

--- BotScript/src/BotScript.cpp
+++ BotScript/src/BotScript.cpp
@@ -1,64 +1,20 @@
 
-//c++ -std=c++11 -O3 -o BotScript.out BotScript.cpp;./BotScript.out
+//$ g++ -std=c++11 -O3 BotScript.cpp -lwiringPi -pthread  include.o variables.o pins.o threads.o builtins.o
 
-#ifndef _BOTSCRIPT_CPP_
-#define _BOTSCRIPT_CPP_ 21931
+#include "include.hpp" //includes basic libraries, functions and classes
+#include "error.hpp"
 
-#ifndef MAIN
-	#define MAIN _BOTSCRIPT_CPP_
-	int main(int argc, char *argv[]);
-#endif
+#include <iostream>
 
-#include "include.cpp" //includes basic libraries, functions and classes
-
-	//list of builtin functions
-const char* builtins[] = {"print", "input", "delay", "clock"};
-const int num_builtins = 4;
+using namespace std;
 
 	//list of defined variables
 vector<variable> variables;
-
-	//returns builtin index (0 = not found)
-uint16_t builtin(string s) {
-	uint8_t i(0);
-	do
-		if(*(builtins + i) == s)
-			return i + 1;
-	while(++i < num_builtins);
-	return 0;
-}
-
-	//handle builtin call by index
-var callBuiltin(uint8_t index, var param) {
-	switch(index) {
-		case 1: //print
-			cout << "in progress"; //TODO param->value;
-		break;
-		case 2: { //input
-			cout << "in progress"; //TODO param->value;
-			string inp;
-			cin >> inp;
-			format(&inp);
-			return new variable(&inp, STR);
-		}
-		case 3: //delay
-			delay(*param->getInt());
-		break;
-		case 4: { //clock
-			uint32_t time = (int)(clock() / 1000);
-			return new variable(&time, INT);
-		}
-	}
-	return new variable(nullptr, NIL);
-}
 
 	//scope interpreter
 void* handleScope(var_lst *scope) {
 
 }
-
-
-#if MAIN == _BOTSCRIPT_CPP_
 
 	//initialisation
 int main(int argc, char *argv[]) {
@@ -70,12 +26,12 @@ int main(int argc, char *argv[]) {
 
 	const char* path = argc > 1? argv[1] : "code.bsc";
 	string code = readFile("code.bsc", true);
-	if(code == "") Error::error("file \"%s\" is empty", path);
+	if(code == "") error("file \"%s\" is empty", path);
 
 		//create code scope of content from default or argument file path
 	var_lst main = toCode(&code);
 
-	cout << Variables::stringify(new variable(&main, LST)) << endl;
+	cout << Variables::stringify(new variable(&main, T_TRM)) << endl;
 
 		//execute code
 	handleScope(&main);
@@ -83,7 +39,3 @@ int main(int argc, char *argv[]) {
 	Variables::free();
 	printf("\n");
 }
-
-#endif //MAIN == _BOTSCRIPT_CPP_
-
-#endif //_BOTSCRIPT_CPP_
