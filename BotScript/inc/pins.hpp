8c72ed622cd10e43bfb79a77c8af241626fb9a7c
alex-Symbroson <alex.Symbroson@gmail.com>
2018-05-27 14:40:58 +0200

    added ISPI macro

--- BotScript/inc/pins.hpp
+++ BotScript/inc/pins.hpp
@@ -1,9 +1,9 @@
 
 #ifndef _PINS_HPP_
-#define _PINS_HPP_
+#    define _PINS_HPP_
 
 namespace Pins {
-	void setup();
+    void setup();
 }
 
 #endif //_PINS_CPP_
@@ -12,23 +12,22 @@ namespace Pins {
 #if MAIN == _PINS_CPP_
 
 int main() {
-	setup();
+    setup();
 
-	int ledRed = 0;
-	softPwmCreate(ledRed, 0, 255);
+    int ledRed = 0;
+    softPwmCreate(ledRed, 0, 255);
 
-	for(int i = 0; i<3; i++) {
-		softPwmWrite(ledRed, 255);
-		delay(1000);
-		softPwmWrite(ledRed, 0);
-		delay(1000);
-	}
+    for(int i = 0; i<3; i++) {
+        softPwmWrite(ledRed, 255);
+        delay(1000);
+        softPwmWrite(ledRed, 0);
+        delay(1000);
+    }
 
-	softPwmStop(ledRed);
+    softPwmStop(ledRed);
 
-	return 0;
+    return 0;
 }
 
 #endif //MAIN == _PINS_CPP_
 */
-
--- BotScript/src/pins.cpp
+++ BotScript/src/pins.cpp
@@ -6,22 +6,23 @@
 #define CUSTOM_BEGIN
 #include "macros.hpp"
 
-#include <wiringPi.h>
-#include <softPwm.h>
-
-#define BEGIN(...) BEGIN_1("Pins",__VA_ARGS__)
-#define END(...) BEGIN_1("Pins",__VA_ARGS__)
-
+#if ISPI
+#    include <softPwm.h>
 /* softPwm.h
-	extern int  softPwmCreate (int pin, int value, int range);
-	extern void softPwmWrite  (int pin, int value);
-	extern void softPwmStop   (int pin);
+    extern int  softPwmCreate (int pin, int value, int range);
+    extern void softPwmWrite  (int pin, int value);
+    extern void softPwmStop   (int pin);
 */
+#    include <wiringPi.h>
+#endif
+
+#define BEGIN(...) BEGIN_1("Pins", __VA_ARGS__)
+#define END(...) BEGIN_1("Pins", __VA_ARGS__)
 
 void Pins::setup() {
-	BEGIN();
-    #if ISPI
-	if(wiringPiSetup() == -1) error("setup wiringPi failed !");
-    #endif
-	END();
+    BEGIN();
+#if ISPI
+    if (wiringPiSetup() == -1) error("setup wiringPi failed !");
+#endif
+    END();
 }
