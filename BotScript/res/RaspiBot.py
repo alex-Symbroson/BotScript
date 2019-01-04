a15a60fc637b6e1b14650e39e7a682db52d805ed
alex-Symbroson <alex.Symbroson@gmail.com>
2019-01-04 13:47:32 +0100

    simplified RaspiBot.py - conversions remaining

--- BotScript/res/RaspiBot.py
+++ BotScript/res/RaspiBot.py
@@ -1,82 +1,83 @@
+#!/usr/bin/python2.7
 
+# initialized correctly
 ISBOT = True
+# object containing components
+objs = {}
 
 def warn(msg, type):
     warnings.warn(msg, type)
 
+def m_cleanup():
+    for obj in objs.values:
+        if hasattr(obj, "cleanup"): obj.cleanup()
+    return True
+
+# remove values with greatest difference and calc arithmetic mean
+def arith(foo, vlen, remove):
+    values = [foo() for i in range(vlen)]
+    arm = sum(values) / vlen
+    # calc difference med <-> values
+    diff = [abs(val - arm) for val in values]
+    # remove elements with greatest difference
+    for i in range(remove): values.pop(diff.index(max(diff)))
+    return sum(values) / vlen
+
+
 try:
-    import warnings
-    import raspibot, RPi.GPIO as GPIO
+    import warnings, raspibot, RPi.GPIO as GPIO
     from serial import Serial, PARITY_EVEN
     from smbus import SMBus
+
+    # create components
+    objs = {
+        "GPIO": GPIO,
+        "Button1": raspibot.button(13, 26, 23),
+        "Button2": raspibot.button(19, 20, 24),
+        "Button3": raspibot.button(16, 21, 25),
+        "Display": raspibot.Display(),
+        "SMBus":   raspibot.ADC(SMBus(1)),
+        "Attiny":  raspibot.AttinyProtocol(Serial('/dev/ttyAMA0', 4800, 8, PARITY_EVEN, 2))
+    }
+
+    # initialize components
+    obj = objs["Display"]
+    obj.clear()
+    obj.init()
+    obj.cursor_goto_xy(0, 0)
+
 except ImportError as e:
     ISBOT = False
     warn("\033[0;33mraspibot module couldnt be loaded:\n" + str(e) + "\033[0;37m", Warning)
 
-objs = {}
-methods = {}
 
 if ISBOT:
 
-    def getObj(name, get):
-        o = objs.get(name)
-        if o == None: o = objs[name] = get()
-        return o
-
-    def m_cleanup():
-        for obj in "Button1,Button2,Button3,Display".split(","):
-            obj = objs.get(obj)
-            if obj: obj.cleanup()
-
-        GPIO.cleanup()
-        return True
-
-    def getDisplay():
-        init = objs.get("Display") == None
-        d = getObj("Display", lambda: raspibot.Display())
-        if init:
-            d.clear()
-            d.init()
-            d.cursor_goto_xy(0, 0)
-        return d
-
-    def getButton(i):
-        return getObj("Button" + str(i), lambda:
-            raspibot.Button(*[(13,26,23), (19,20,24), (16,21,25)][i - 1]))
-
-    def getAttiny():
-        return getObj("Attiny", lambda:
-            raspibot.AttinyProtocol(Serial('/dev/ttyAMA0', 4800, 8, PARITY_EVEN, 2)))
-
-    def getADC():
-        return getObj("SMBus", lambda: raspibot.ADC(SMBus(1)))
-
     def m_write(s, x = None, y = None, *args):
-        d = getDisplay()
-        if y != None: d.cursor_goto_xy(int(x), int(y))
-        d.write(s)
+        if y != None: objs["Display"].cursor_goto_xy(int(x), int(y))
+        objs["Display"].write(s)
 
     methods = {
-        "write": m_write,
         "cleanup": m_cleanup,
+        "write": m_write,
         # Serial (Attiny)
-        "getEncoders": lambda : getAttiny().get_encoders(),
-        "resetEncoders": lambda : getAttiny().reset_encoders(),
-        "stopMotors": lambda : getAttiny().stop_motors(),
-        "setMotors": lambda l, r: getAttiny().set_motors(l, r), # left, right
-        "setBuzzer": lambda f, d, v: getAttiny().set_buzzer(f, d, v), # frequency, duration, volume
-        "stopBuzzer": lambda : getAttiny().stop_buzzer(),
+        "setMotors":    lambda l, r: objs["Attiny"].set_motors(l, r), # left, right
+        "setBuzzer":    lambda f, d, v: objs["Attiny"].set_buzzer(f, d, v), # frequency, duration, volume
+        "getEncoders":  lambda : objs["Attiny"].get_encoders(),
+        "resetEncoders":lambda : objs["Attiny"].reset_encoders(),
+        "stopMotors":   lambda : objs["Attiny"].stop_motors(),
+        "stopBuzzer":   lambda : objs["Attiny"].stop_buzzer(),
         # ADC
-        "getSharp": lambda i: getADC().read_channel(i - 1), # 1 - 3
-        "getBattery": lambda i: getADC().read_channel(4),
+        "getSharp":     arith(lambda i: objs["ADC"].read_channel(i), 8, 4), # 1 - 2
+        "getBattery":   lambda i: objs["ADC"].read_channel(4),
         # Buttons
-        "setRedLED": lambda i, v: getButton(i).setRedLED(int(v)),
-        "setGreenLED": lambda i, v: getButton(i).setGreenLED(int(v)),
-        "waitForBtnPress": lambda i: getButton(i).waitForButtonPress(),
-        "waitForBtnRelease": lambda i: getButton(i).waitForButtonRelease(),
-        "waitForBtn": lambda i: getButton(i).waitForButton(),
-        "isBtnPressed": lambda i: getButton(i).isPressed(),
-        "isBot": lambda : True
+        "setRedLED":    lambda  i, v: objs["Button%i" % i].setRedLED(int(v)),
+        "setGreenLED":  lambda  i, v: objs["Button%i" % i].setGreenLED(int(v)),
+        "waitForBtnPress":  lambda i: objs["Button%i" % i].waitForButtonPress(),
+        "waitForBtnRelease":lambda i: objs["Button%i" % i].waitForButtonRelease(),
+        "waitForBtn":       lambda i: objs["Button%i" % i].waitForButton(),
+        "isBtnPressed":     lambda i: objs["Button%i" % i].isPressed(),
+        "isBot":            lambda  : True
     }
 else:
     methods = {
